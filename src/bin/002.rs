635e2d3dd4e44edb947fd3e7cc28d7dae3e4dd68
UseK <use.k.james@gmail.com>
2021-05-02 14:52:28 +0900

    Accept 002 - Encyclopedia of Parentheses

--- src/bin/002.rs
+++ src/bin/002.rs
@@ -5,13 +5,13 @@ use competitive_tools_rust::io::*;
 fn main() {
     let n: u32 = parse_line();
     for i in 1..2_usize.pow(n - 1) {
-        //d!(i);
-        let bits = i.to_bit_vec(n as usize);
-        //d!(bits);
+        let bits: Vec<bool> = i.to_bit_vec(n as usize).into_iter().rev().collect();
         if is_correct(&bits) {
+            //d!(i);
+            //d!(bits);
             let parentheses: String = bits
                 .into_iter()
-                .map(|bit| if bit { '(' } else { ')' })
+                .map(|bit| if !bit { '(' } else { ')' })
                 .collect();
             println!("{}", parentheses);
         }
@@ -19,15 +19,15 @@ fn main() {
 }
 
 fn is_correct(bits: &[bool]) -> bool {
-    let mut true_count = 0;
+    let mut false_count = 0;
     for &bit in bits {
-        if bit {
-            true_count += 1;
-        } else if true_count == 0 {
+        if !bit {
+            false_count += 1;
+        } else if false_count == 0 {
             return false;
         } else {
-            true_count -= 1;
+            false_count -= 1;
         }
     }
-    true_count == 0
+    false_count == 0
 }
