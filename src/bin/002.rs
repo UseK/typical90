1c665ceb6079169ee325bb61e9038776551c6606
UseK <use.k.james@gmail.com>
2021-05-02 14:32:36 +0900

    Snapshot: fail test 4 in 002 - Encyclopedia of Parentheses

--- src/bin/002.rs
+++ src/bin/002.rs
@@ -1,5 +1,33 @@
+use competitive_tools_rust::bitset::Bitset as _;
+//use competitive_tools_rust::d;
 use competitive_tools_rust::io::*;
 
 fn main() {
-    unimplemented!();
+    let n: u32 = parse_line();
+    for i in 1..2_usize.pow(n - 1) {
+        //d!(i);
+        let bits = i.to_bit_vec(n as usize);
+        //d!(bits);
+        if is_correct(&bits) {
+            let parentheses: String = bits
+                .into_iter()
+                .map(|bit| if bit { '(' } else { ')' })
+                .collect();
+            println!("{}", parentheses);
+        }
+    }
+}
+
+fn is_correct(bits: &[bool]) -> bool {
+    let mut true_count = 0;
+    for &bit in bits {
+        if bit {
+            true_count += 1;
+        } else if true_count == 0 {
+            return false;
+        } else {
+            true_count -= 1;
+        }
+    }
+    true_count == 0
 }
