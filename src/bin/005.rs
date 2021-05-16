8ecb3906766b4a8a3f9c73d61f28bde8e7ae4916
UseK <use.k.james@gmail.com>
2021-05-16 23:11:41 +0900

    RE -> TLE in 005 - Restricted Digits

--- src/bin/005.rs
+++ src/bin/005.rs
@@ -1,4 +1,3 @@
-// use competitive_tools_rust::d;
 use competitive_tools_rust::io::*;
 
 const LAW: usize = 1_000_000_007;
@@ -6,20 +5,22 @@ const LAW: usize = 1_000_000_007;
 fn main() {
     let (n, b, k): (usize, usize, usize) = parse_tuple3();
     let c: Vec<usize> = parse_values(k);
-    let mut dp: Vec<Vec<usize>> = vec![vec![0; b]; n];
-    for &c_item in &c {
-        dp[0][c_item % b] += 1;
-    }
-    for i in 1..n {
+    let mut dp: Vec<usize> = vec![0; b];
+    c.iter().for_each(|&c_item| {
+        dp[c_item % b] += 1;
+    });
+
+    let ans: Vec<usize> = (1..n).fold(dp, |acc, _| {
+        let mut new_acc = vec![0; b];
         for j in 0..b {
             for &c_item in &c {
                 let new_mod = (j * 10 + c_item) % b;
                 // d!(i, j, c_item, new_mod, dp[i - 1][j]);
-                dp[i][new_mod] += dp[i - 1][j];
-                dp[i][new_mod] %= LAW;
+                new_acc[new_mod] += acc[j];
+                new_acc[new_mod] %= LAW;
             }
         }
-    }
-    // dp.iter().for_each(|item| println!("{:?}", item));
-    println!("{}", dp[n - 1][0]);
+        new_acc
+    });
+    println!("{}", ans[0]);
 }
