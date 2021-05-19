7c6de020d701adfe34636b54acc6a6f048705c7b
UseK <use.k.james@gmail.com>
2021-05-19 18:58:05 +0900

    Accept 008 - AtCounter

--- src/bin/008.rs
+++ src/bin/008.rs
@@ -1,5 +1,37 @@
 use competitive_tools_rust::io::*;
 
+const ATCODER: [char; 7] = ['a', 't', 'c', 'o', 'd', 'e', 'r'];
+const LAW: usize = 1_000_000_007;
+
 fn main() {
-    unimplemented!();
+    let n: usize = parse_line();
+    let s: Vec<char> = parse_line::<String>().chars().collect();
+    //println!("{:?}", s);
+    let mut dp = [
+        vec![0; n],
+        vec![0; n],
+        vec![0; n],
+        vec![0; n],
+        vec![0; n],
+        vec![0; n],
+        vec![0; n],
+    ];
+    let mut current = 0;
+    for i in 0..n {
+        if s[i] == ATCODER[0] {
+            current += 1;
+        }
+        dp[0][i] = current;
+    }
+    for i in 1..7 {
+        for j in 1..n {
+            dp[i][j] = if s[j] == ATCODER[i] {
+                (dp[i][j - 1] + dp[i - 1][j - 1]) % LAW
+            } else {
+                dp[i][j - 1] % LAW
+            }
+        }
+    }
+    //dp.iter().for_each(|item| println!("{:?}", item));
+    println!("{}", dp[6][n - 1]);
 }
