7d44c2d2857372e2abcd300c44d708a61be4c955
UseK <use.k.james@gmail.com>
2021-06-15 01:28:16 +0900

    Accept 014 - We Used to Sing a Song Together

--- src/bin/014.rs
+++ src/bin/014.rs
@@ -1,5 +1,15 @@
 use competitive_tools_rust::io::*;
+use competitive_tools_rust::math::AbsDiff;
 
 fn main() {
-    unimplemented!();
+    let n: usize = parse_line();
+    let mut a: Vec<usize> = parse_values(n);
+    let mut b: Vec<usize> = parse_values(n);
+    a.sort_unstable();
+    b.sort_unstable();
+    let ans = a
+        .iter()
+        .zip(b.iter())
+        .fold(0, |acc, (a_item, b_item)| acc + a_item.abs_diff(*b_item));
+    println!("{}", ans);
 }
