346d4da83716e44ed5b2eda5258c5293de255181
UseK <use.k.james@gmail.com>
2021-06-15 02:15:46 +0900

    Accept 016 - Minimum Coins

--- src/bin/016.rs
+++ src/bin/016.rs
@@ -1,5 +1,6 @@
-use competitive_tools_rust::d;
 use competitive_tools_rust::io::*;
+
+const MAX_COUNT: usize = 9999;
 
 fn main() {
     let n: usize = parse_line();
@@ -9,37 +10,22 @@ fn main() {
     let less = coins[0];
     let mid = coins[1];
     let bigger = coins[2];
-    d!(less, mid, bigger);
 
-    if n % bigger == 0 {
-        println!("{}", n / bigger);
-        return;
-    }
-
-    let max_bigger_count = (n / bigger).min(9999);
-
-    for bigger_count in (0..=max_bigger_count).rev() {
-        d!(bigger_count);
-        let remains_1 = n - bigger_count * bigger;
-        d!(remains_1);
-        //println!();
-        if remains_1 % mid == 0 {
-            println!("{}", bigger_count + remains_1 / mid);
-            return;
+    let mut ans = MAX_COUNT;
+    for bigger_count in 0..=MAX_COUNT {
+        if bigger_count * bigger > n {
+            break;
         }
-
-        let max_mid_count = (remains_1 / mid).min(9999);
-
-        for mid_count in (0..=max_mid_count).rev() {
-            d!(mid_count);
-            let remains_2 = remains_1 - mid_count * mid;
-            d!(remains_2);
-            println!();
-
-            if remains_2 % less == 0 {
-                println!("{}", bigger_count + mid_count + remains_2 / less);
-                return;
+        let remain_1 = n - bigger * bigger_count;
+        for mid_count in 0..=MAX_COUNT {
+            if mid_count * mid > remain_1 {
+                break;
+            }
+            let remain_2 = remain_1 - mid * mid_count;
+            if remain_2 % less == 0 {
+                ans = ans.min(bigger_count + mid_count + remain_2 / less);
             }
         }
     }
+    println!("{}", ans);
 }
