63a64fdc78cb8e99978f89633a47b8c877bcabf7
UseK <use.k.james@gmail.com>
2021-06-21 21:05:57 +0900

    Accept 024 - Select +／- One

--- src/bin/024.rs
+++ src/bin/024.rs
@@ -1,5 +1,19 @@
-use competitive_tools_rust::io::*;
+use competitive_tools_rust::{io::*, math::AbsDiff};
 
 fn main() {
-    unimplemented!();
+    let (n, k): (usize, usize) = parse_tuple2();
+    let a: Vec<usize> = parse_values(n);
+    let b: Vec<usize> = parse_values(n);
+    let diff_sum = a
+        .iter()
+        .zip(b.iter())
+        .fold(0, |acc, (x, y)| acc + x.abs_diff(*y));
+    println!(
+        "{}",
+        if diff_sum <= k && (k - diff_sum) % 2 == 0 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
