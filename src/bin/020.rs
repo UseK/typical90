342b426d0a14a60a29ed4dcd19b41f77a2238746
UseK <use.k.james@gmail.com>
2021-06-21 19:07:59 +0900

    Accept 020 - Log Inequality

--- src/bin/020.rs
+++ src/bin/020.rs
@@ -1,5 +1,6 @@
 use competitive_tools_rust::io::*;
 
 fn main() {
-    unimplemented!();
+    let (a, b, c): (usize, usize, usize) = parse_tuple3();
+    println!("{}", if a < c.pow(b as u32) { "Yes" } else { "No" });
 }
