c902e47972429868ea37554dfff12e288b1eed8d
UseK <use.k.james@gmail.com>
2021-06-29 04:47:08 +0900

    Accept 026 - Independent Set on a Tree with competitive_tools_rust

--- src/bin/026.rs
+++ src/bin/026.rs
@@ -1,41 +1,5 @@
+use competitive_tools_rust::graph::BipartiteGraph;
 use competitive_tools_rust::io::*;
-
-pub trait BipartiteGraph {
-    fn bi_partition(&self) -> (Vec<usize>, Vec<usize>);
-}
-
-pub trait Tree: BipartiteGraph {}
-
-impl BipartiteGraph for Vec<Vec<usize>> {
-    fn bi_partition(&self) -> (Vec<usize>, Vec<usize>) {
-        let mut current: Vec<Option<bool>> = vec![None; self.len()];
-        let mut stack: Vec<(usize, bool)> = vec![];
-        current[0] = Some(true);
-        stack.append(&mut self[0].iter().map(|&i| (i, false)).collect());
-
-        while let Some((vertex_id, bi)) = stack.pop() {
-            current[vertex_id] = Some(bi);
-            let nexts = self[vertex_id]
-                .iter()
-                .filter(|&&i| current[i].is_none())
-                .map(|&i| (i, !bi));
-            stack.append(&mut nexts.collect());
-        }
-        // println!("{:?}", current);
-        let mut x: Vec<usize> = vec![];
-        let mut y: Vec<usize> = vec![];
-        for (ind, bi) in current.iter().enumerate() {
-            if bi.unwrap() {
-                x.push(ind);
-            } else {
-                y.push(ind);
-            }
-        }
-        (x, y)
-    }
-}
-
-impl Tree for Vec<Vec<usize>> {}
 
 fn ans(list: Vec<usize>, n: usize) {
     println!(
@@ -55,7 +19,6 @@ fn main() {
         let (a, b): (usize, usize) = parse_tuple2();
         adjacency_list[a - 1].push(b - 1);
         adjacency_list[b - 1].push(a - 1);
-        // println!("{:?}", adjacency_list);
     }
     let (x, y) = adjacency_list.bi_partition();
     if x.len() <= y.len() {
