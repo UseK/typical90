b926c7c079bac9a326f1cedaef585cf2a2a7b279
UseK <use.k.james@gmail.com>
2021-06-29 04:54:29 +0900

    Accept 027 - Sign Up Requests

--- src/bin/027.rs
+++ src/bin/027.rs
@@ -1,5 +1,12 @@
 use competitive_tools_rust::io::*;
+use std::collections::HashSet;
 
 fn main() {
-    unimplemented!();
+    let n: usize = parse_line();
+    let mut s = HashSet::new();
+    for i in 0..n {
+        if s.insert(parse_line::<String>()) {
+            println!("{}", i + 1);
+        }
+    }
 }
