5b36c1e74ead48c82da9bcdcb5df7be8f2965992
UseK <use.k.james@gmail.com>
2021-07-05 07:59:09 +0900

    Snapshot: WA 4 in 032

---
+++ src/bin/032.rs
@@ -0,0 +1,44 @@
+// use competitive_tools_rust::d;
+use competitive_tools_rust::io::{parse_line, parse_tuple2, parse_values};
+use itertools::Itertools;
+
+fn main() {
+    let n: usize = parse_line();
+    let a: Vec<Vec<usize>> = (0..n).map(|_| parse_values(n)).collect();
+    let m: usize = parse_line();
+    let xy: Vec<(usize, usize)> = (0..m).map(|_| parse_tuple2::<usize>()).collect();
+    let mut ng_matrix = vec![vec![false; n]; n];
+    for (x, y) in xy {
+        ng_matrix[x - 1][y - 1] = true;
+        ng_matrix[y - 1][x - 1] = true;
+    }
+
+    // println!("{:?}", a);
+    // println!();
+    // println!();
+    // println!("{:?}", ng_matrix);
+
+    let mut min_score: Option<usize> = None;
+    (0..n).permutations(n).for_each(|pattern| {
+        // println!("{:?}", pattern);
+        let is_ok = pattern
+            .iter()
+            .tuple_windows::<(&usize, &usize)>()
+            .all(|(&a, &b)| !ng_matrix[a][b]);
+        if is_ok {
+            let score = pattern
+                .iter()
+                .enumerate()
+                .fold(0, |acc, (ind, &p_item)| acc + a[ind][p_item]);
+            // d!(score);
+            let current = min_score.unwrap_or(score);
+            min_score = Some(current.min(score));
+            // d!(min_score);
+        }
+    });
+
+    println!(
+        "{}",
+        min_score.map_or("-1".to_string(), |score| score.to_string())
+    );
+}
---
+++ test-local.sh
@@ -0,0 +1,8 @@
+cargo build --release --bin "$1"
+# shellcheck disable=SC2038
+# find "./testcase/$1/in/" -type f -name "*.txt"
+# shellcheck disable=SC2038
+mkdir -p ./testcase/"$1"/my_ans
+ls "./testcase/$1/in/" | xargs -I {} sh -c "./target/release/$1 < ./testcase/$1/in/{} > ./testcase/$1/my_ans/{}; echo generated ./testcase/$1/my_ans/{}"
+# find "./testcase/$1/in/" -type f -name "*.txt"| xargs -I {} sh -c "./target/debug/$1 < {} > ./testcase/$1/my_ans"
+ls "./testcase/$1/in/" | xargs -I {} sh -c "echo Difference in {}; diff --strip-trailing-cr ./testcase/$1/out/{} ./testcase/$1/my_ans/{}; echo;"
