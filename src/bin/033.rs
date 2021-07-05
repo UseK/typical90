875d216c158f67c002dedfc041e801c7959a0b1a
UseK <use.k.james@gmail.com>
2021-07-05 21:53:54 +0900

    Accept 033 - Not Too Bright

--- src/bin/033.rs
+++ src/bin/033.rs
@@ -2,5 +2,10 @@ use competitive_tools_rust::io::parse_tuple2;
 
 fn main() {
     let (h, w): (usize, usize) = parse_tuple2();
-    println!("{}", ((h + 1) / 2) * ((w + 1) / 2));
+    let ans = match (h, w) {
+        (1, w) => w,
+        (h, 1) => h,
+        (h, w) => ((h + 1) / 2) * ((w + 1) / 2),
+    };
+    println!("{}", ans);
 }
