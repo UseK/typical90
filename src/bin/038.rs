23e7932d953e19c55277dadf1bd5e38da0cbdb0e
UseK <use.k.james@gmail.com>
2021-07-07 20:04:41 +0900

    Accept 038 - Large LCM without u128

--- src/bin/038.rs
+++ src/bin/038.rs
@@ -2,11 +2,12 @@ use competitive_tools_rust::{io::parse_tuple2, math::Gcd};
 
 fn main() {
     let (a, b): (usize, usize) = parse_tuple2();
-    let gcd = a.gcd(b) as u128;
-    let ans: u128 = (a as u128) / gcd * (b as u128);
-    if ans > 10u128.pow(18) {
-        println!("Large");
+    let gcd = a.gcd(b);
+    let threshold = 10usize.pow(18);
+    let tmp = b / gcd;
+    if a <= threshold / tmp {
+        println!("{}", tmp * a);
     } else {
-        println!("{}", ans);
+        println!("Large");
     }
 }
