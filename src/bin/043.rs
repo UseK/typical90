be1709f8632a82051d381519a685d90c58581cb1
UseK <use.k.james@gmail.com>
2021-07-11 14:47:49 +0900

    Snapshot: still WA in test-local 043

--- src/bin/043.rs
+++ src/bin/043.rs
@@ -1,6 +1,7 @@
 // use competitive_tools_rust::d;
 use competitive_tools_rust::io::{parse_line, parse_tuple2};
-use std::collections::VecDeque;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 
 fn main() {
     let (h, w): (usize, usize) = parse_tuple2();
@@ -13,18 +14,20 @@ fn main() {
     let maze = maze;
     // maze.iter().for_each(|item| println!("{:?}", item));
 
-    let mut counter: Vec<Vec<usize>> = vec![vec![usize::MAX; w + 2]; h + 2];
+    let mut already: Vec<Vec<bool>> = vec![vec![false; w + 2]; h + 2];
     // let mut togo_stock: Vec<ToGo> = ToGo::new_all_directions(start_r, start_c, 0);
-    let mut togo_queue: VecDeque<ToGo> = VecDeque::new();
+    // let mut togo_queue: VecDeque<ToGo> = VecDeque::new();
+    let mut togo_queue: BinaryHeap<Reverse<ToGo>> = BinaryHeap::new();
     togo_queue.append(
         &mut ToGo::new_all_directions(start_r, start_c, 0)
             .into_iter()
+            .map(Reverse)
             .collect(),
     );
     loop {
-        let togo = togo_queue.pop_front().unwrap();
-        counter[togo.r][togo.c] = counter[togo.r][togo.c].min(togo.count);
-
+        // let togo = togo_queue.pop_front().unwrap();
+        let Reverse(togo) = togo_queue.pop().unwrap();
+        // println!("{:?}", togo);
         let (next_r, next_c) = match togo.direction {
             Direction::Up => (togo.r - 1, togo.c),
             Direction::Down => (togo.r + 1, togo.c),
@@ -36,15 +39,25 @@ fn main() {
             println!("{}", togo.count);
             return;
         }
+        if already[next_r][next_c] {
+            continue;
+        } else {
+            already[next_r][next_c] = true;
+        }
         if maze[next_r][next_c] {
+            togo_queue.push(Reverse(ToGo::new(
+                next_r,
+                next_c,
+                togo.direction,
+                togo.count,
+            )));
             let next_togo_list = match togo.direction {
                 Direction::Up => ToGo::new_horizontal_directions(next_r, next_c, togo.count + 1),
                 Direction::Down => ToGo::new_horizontal_directions(next_r, next_c, togo.count + 1),
                 Direction::Left => ToGo::new_vertical_directions(next_r, next_c, togo.count + 1),
                 Direction::Right => ToGo::new_vertical_directions(next_r, next_c, togo.count + 1),
             };
-            togo_queue.push_back(ToGo::new(next_r, next_c, togo.direction, togo.count));
-            togo_queue.append(&mut next_togo_list.into_iter().collect());
+            togo_queue.append(&mut next_togo_list.into_iter().map(Reverse).collect());
 
             // println!("{:?}", togo);
             // println!("----------");
@@ -54,7 +67,7 @@ fn main() {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 enum Direction {
     Up,
     Down,
@@ -62,21 +75,21 @@ enum Direction {
     Right,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct ToGo {
+    count: usize,
     r: usize,
     c: usize,
     direction: Direction,
-    count: usize,
 }
 
 impl ToGo {
     fn new(r: usize, c: usize, direction: Direction, count: usize) -> Self {
         ToGo {
+            count,
             r,
             c,
             direction,
-            count,
         }
     }
 
