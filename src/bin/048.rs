ee23704d3952b9f191200c0a02289d0379728e1a
UseK <use.k.james@gmail.com>
2021-07-12 03:15:25 +0900

    Accept 048 - I will not drop out

---
+++ src/bin/048.rs
@@ -0,0 +1,17 @@
+use competitive_tools_rust::io::parse_tuple2;
+
+fn main() {
+    let (n, k): (usize, usize) = parse_tuple2();
+    let ab: Vec<(usize, usize)> = (0..n).map(|_| parse_tuple2::<usize>()).collect();
+    // println!("{:?}", ab);
+    let mut scores = ab.iter().fold(vec![], |mut acc, ab_item| {
+        acc.push(ab_item.1);
+        acc.push(ab_item.0 - ab_item.1);
+        acc
+    });
+    // println!("{:?}", scores);
+    scores.sort_unstable();
+    scores.reverse();
+    // println!("{:?}", scores);
+    println!("{:?}", &scores[0..k].iter().sum::<usize>());
+}
