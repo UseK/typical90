cba6cd3e65f9ec985fdb52c2ade3e59db942798c
UseK <use.k.james@gmail.com>
2021-07-13 02:10:20 +0900

    Accept 055 - Select 5

--- src/bin/055.rs
+++ src/bin/055.rs
@@ -1,15 +1,26 @@
 use competitive_tools_rust::io::{parse_tuple3, parse_values};
-use itertools::Itertools;
 
 fn main() {
     let (n, p, q): (usize, usize, usize) = parse_tuple3();
     let a: Vec<usize> = parse_values(n);
     let mut ans = 0;
-    a.into_iter().combinations(5).for_each(|pattern| {
-        let remainder = pattern.into_iter().reduce(|acc, p_item| (acc * p_item) % p);
-        if remainder.unwrap() == q {
-            ans += 1;
+
+    for i in 0..n {
+        for j in 0..i {
+            let remain_1 = (a[i] * a[j]) % p;
+            for k in 0..j {
+                let remain_2 = (remain_1 * a[k]) % p;
+                for l in 0..k {
+                    let remain_3 = (remain_2 * a[l]) % p;
+                    for m in 0..l {
+                        if (remain_3 * a[m]) % p == q {
+                            ans += 1;
+                        }
+                    }
+                }
+            }
         }
-    });
+    }
+
     println!("{}", ans);
 }
