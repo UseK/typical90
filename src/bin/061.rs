7963d9b995ee8a068c68ecf7a7eb4a15044814a2
UseK <use.k.james@gmail.com>
2021-07-15 02:57:26 +0900

    Revert "TLE 061 - Deck without VecDequque but with Vec"
    This reverts commit 5cf6f355df41038ba2df75a843e560d9df6398b1.

--- src/bin/061.rs
+++ src/bin/061.rs
@@ -1,13 +1,14 @@
 use competitive_tools_rust::io::{parse_line, parse_tuple2};
+use std::collections::VecDeque;
 
 fn main() {
     let q: usize = parse_line();
-    let mut queue: Vec<usize> = vec![];
+    let mut queue: VecDeque<usize> = VecDeque::new();
     for _ in 0..q {
         let (t, x): (usize, usize) = parse_tuple2();
         match t {
-            1 => queue.insert(0, x),
-            2 => queue.push(x),
+            1 => queue.push_front(x),
+            2 => queue.push_back(x),
             3 => {
                 println!("{}", queue[x - 1]);
             }
