b57db6fd002e6f7ebc1304a19a3cf9e499f33614
UseK <use.k.james@gmail.com>
2021-07-24 04:09:39 +0900

    Move PowMod to competitive_tools_rust in 069 - Colorful Blocks 2

--- src/bin/069.rs
+++ src/bin/069.rs
@@ -1,5 +1,5 @@
-use competitive_tools_rust::bitset::Bitset;
 use competitive_tools_rust::io::parse_tuple2;
+use competitive_tools_rust::math::ModPow;
 
 const LAW: usize = 10usize.pow(9) + 7;
 
@@ -19,73 +19,4 @@ fn main() {
     }
 
     println!("{}", k * (k - 1) % LAW * (k - 2).mod_pow(n - 2, LAW) % LAW);
-}
-
-trait ModPow {
-    /// Returns Modular Exponentiation  with
-    fn mod_pow(self, exp: Self, m: Self) -> Self;
-}
-
-impl ModPow for usize {
-    fn mod_pow(self, exp: Self, m: Self) -> Self {
-        let mut acc = 1;
-        let mut current_mod_pow = self % m;
-        for i in 0..exp {
-            if 2usize.pow(i as u32) > exp {
-                break;
-            }
-            if exp.is_bit_on(i) {
-                acc = (acc * current_mod_pow) % m;
-            }
-            current_mod_pow = (current_mod_pow * current_mod_pow) % m;
-        }
-        acc
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::ModPow;
-    const LAW: usize = 10usize.pow(9) + 7;
-
-    fn naive_modular_exponentiation(b: usize, e: usize, m: usize) -> usize {
-        let mut exp = 1;
-        for _ in 0..e {
-            exp = (exp * b) % m;
-        }
-        exp
-    }
-
-    #[test]
-    fn test_mod_pow_in_small_case() {
-        let b = 2;
-        let exp = 10;
-        assert_eq!(b.mod_pow(exp, 5), 4);
-    }
-
-    #[test]
-    fn test_mod_pow_in_small_case2() {
-        let b = 2;
-        let exp = 4;
-        assert_eq!(b.mod_pow(exp, 5), 1);
-    }
-
-    #[test]
-    fn test_mod_pow_in_big_case() {
-        let b = 2019;
-        let exp = 615;
-        assert_eq!(b.mod_pow(exp, LAW), 492000830);
-        assert_eq!(naive_modular_exponentiation(b, exp, LAW), 492000830);
-    }
-
-    #[test]
-    fn test_mod_pow_in_all_pattern() {
-        for b in 0..100 {
-            for exp in 0..100 {
-                for m in 2..100 {
-                    assert_eq!(b.mod_pow(exp, m), naive_modular_exponentiation(b, exp, m));
-                }
-            }
-        }
-    }
 }
