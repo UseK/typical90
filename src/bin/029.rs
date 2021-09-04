4a734f5c8180f284c035228c6c7dcd38b6f0098e
UseK <use.k.james@gmail.com>
2021-09-04 14:29:09 +0900

    Snapshot: just implement coordinate_compression()

--- src/bin/029.rs
+++ src/bin/029.rs
@@ -1,33 +1,35 @@
-// use competitive_tools_rust::d;
+use competitive_tools_rust::d;
 use competitive_tools_rust::io::*;
 
 fn main() {
-    let (w, n): (usize, usize) = parse_tuple2();
-    let mut stack: Vec<Vec<bool>> = vec![vec![true; w], vec![false; w]];
-    for _ in 0..n {
-        let (l, r): (usize, usize) = parse_tuple2();
-        for h in (1..stack.len()).rev() {
-            let is_vacant = stack[h][l - 1..r].iter().all(|&x| !x);
-            let can_put = stack[h - 1][l - 1..r].iter().any(|&x| x);
-            if is_vacant && can_put {
-                for i in l - 1..r {
-                    stack[h][i] = true;
-                }
-                if h == stack.len() - 1 {
-                    stack.push(vec![false; w]);
-                }
-                println!("{}", h);
-                break;
-            }
-        }
-        // show_maze(&stack);
-    }
+    let (_w, n): (usize, usize) = parse_tuple2();
+    let lr: Vec<(usize, usize)> = (0..n).map(|_| parse_tuple2()).collect();
+    let compressed_lr = compress(lr);
+    d!(compressed_lr);
 }
 
-// fn show_maze(maze: &[Vec<bool>]) {
-//     maze.iter().for_each(|line| {
-//         let line_str: String = line.iter().map(|&b| if b { '#' } else { '.' }).collect();
-//         println!("{}", line_str);
-//     });
-//     println!("{}", (0..maze[0].len()).map(|_| '-').collect::<String>());
-// }
+fn compress(lr: Vec<(usize, usize)>) -> Vec<(usize, usize)> {
+    d!(lr);
+    let values: Vec<usize> = lr.iter().fold(vec![], |mut acc, (l, r)| {
+        acc.push(*l);
+        acc.push(*r);
+        acc
+    });
+    let compression_map = coordinate_compression(values);
+    let compressed_lr: Vec<(usize, usize)> = lr
+        .iter()
+        .map(|(l, r)| (compression_map[*l].unwrap(), compression_map[*r].unwrap()))
+        .collect();
+    compressed_lr
+}
+
+fn coordinate_compression(mut values: Vec<usize>) -> Vec<Option<usize>> {
+    values.sort_unstable();
+    values.iter().enumerate().fold(
+        vec![None; values[values.len() - 1] + 1],
+        |mut acc, (ind, &v)| {
+            acc[v] = Some(ind);
+            acc
+        },
+    )
+}
