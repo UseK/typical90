afcdf83551a88bfe9bbdb5506e587a09a12d40c3
UseK <use.k.james@gmail.com>
2021-10-03 13:36:05 +0900

    Accept 075 - Magic For Ball

---
+++ src/bin/075.rs
@@ -0,0 +1,15 @@
+use competitive_tools_rust::io::*;
+use competitive_tools_rust::math::prime_factors;
+
+fn main() {
+    let n: usize = parse_line();
+    let n_factors = prime_factors(n).len();
+    // println!("{}", n_factors);
+    for i in 0..2u32.pow(12) {
+        // println!("i:{}", i);
+        if 2usize.pow(i) >= n_factors {
+            println!("{}", i);
+            return;
+        }
+    };
+}
