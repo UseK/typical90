549d266d9f3579ee2867aa5764cb223609b8118a
UseK <use.k.james@gmail.com>
2021-10-07 03:37:10 +0900

    Accept 006 with Segment Tree

--- src/bin/006.rs
+++ src/bin/006.rs
@@ -1,40 +1,25 @@
 //use competitive_tools_rust::d;
 use competitive_tools_rust::io::*;
+use competitive_tools_rust::segment_tree::SegmentTree;
 
 fn main() {
     let (n, k): (usize, usize) = parse_tuple2();
-    let s: Vec<char> = parse_line::<String>().chars().collect();
+    let s: String = parse_line();
+    let cs: Vec<char> = s.chars().collect();
+    let mut seg: SegmentTree<(char, usize)> = SegmentTree::new(n, ('z', usize::MAX));
+    (0..n).for_each(|i| {
+        seg.update(i, (cs[i], i));
+    });
+    // println!("{:?}", seg.tree);
 
-    let most_left_indices = gen_most_left_indices(n, &s);
-
-    let mut current_ind = 0;
-    let ans: String = (1..=k)
-        .rev()
-        .map(|current_k| {
-            // d!(current_k);
-            let ans_char = ('a'..='z')
-                .find(|&c| n - most_left_indices[c as usize - 97][current_ind] >= current_k)
-                .unwrap();
-            // d!(current_k, ans_char, current_ind);
-            current_ind = most_left_indices[ans_char as usize - 97][current_ind] + 1;
-            // d!(current_k, ans_char, current_ind);
-            ans_char
+    let mut left = 0;
+    let answer: String = (n - k + 1..=n)
+        .map(|right| {
+            let ans = seg.query(left, right);
+            // println!("{:?}", ans);
+            left = ans.1 + 1;
+            ans.0
         })
         .collect();
-    println!("{}", ans);
-}
-
-fn gen_most_left_indices(n: usize, s: &[char]) -> Vec<Vec<usize>> {
-    let mut most_left_indices: Vec<Vec<usize>> = vec![vec![n; n]; 26];
-    for c in 'a'..='z' {
-        let most_left_ind = &mut most_left_indices[c as usize - 97];
-        let mut current_ind = n;
-        for s_ind in (0..n).rev() {
-            if s[s_ind] == c {
-                current_ind = s_ind
-            }
-            most_left_ind[s_ind] = current_ind;
-        }
-    }
-    most_left_indices
+    println!("{}", answer);
 }
