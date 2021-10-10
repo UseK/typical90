aca2421bef71f9c0cdb0d455632b36802a1c17f4
UseK <use.k.james@gmail.com>
2021-10-10 15:09:08 +0900

    Accept 076 - Cake Cut

--- src/bin/076.rs
+++ src/bin/076.rs
@@ -1,30 +1,37 @@
+// use competitive_tools_rust::d;
 use competitive_tools_rust::io::{parse_line, parse_values};
 
 fn main() {
     let n: usize = parse_line();
     let a: Vec<usize> = parse_values(n);
+    let whole_size: usize = a.iter().sum();
+    // d!(whole_size);
     let mut start: usize = 0;
     let mut end: usize = 0;
     let mut sum: usize = 0;
     loop {
-        match (sum * 10).cmp(&n) {
+        // d!(start, end, sum * 10);
+        match (sum * 10).cmp(&whole_size) {
             std::cmp::Ordering::Equal => {
-                if sum * 10 == n {
-                    println!("Yes");
-                    return;
-                }
+                println!("Yes");
+                return;
             }
             std::cmp::Ordering::Less => {
+                // d!("Less!");
                 sum += a[end];
                 end += 1;
                 if end >= n {
-                    println!("No");
-                    return;
+                    end = 0;
                 }
             }
             std::cmp::Ordering::Greater => {
+                // d!("Greater!");
                 sum -= a[start];
                 start += 1;
+                if start >= n {
+                    println!("No");
+                    return;
+                }
             }
         }
     }
