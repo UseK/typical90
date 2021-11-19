8d205c76b323b1cb1bb2792e938b2c7914227a8f
UseK <use.k.james@gmail.com>
2021-11-20 02:52:32 +0900

    Accept 085 - Multiplication 085

--- src/bin/085.rs
+++ src/bin/085.rs
@@ -1,35 +1,35 @@
-use std::collections::BTreeSet;
-
-use competitive_tools_rust::{io::parse_line, math::prime_factors};
+use competitive_tools_rust::{io::parse_line};
 
 fn main() {
     let k: usize = parse_line();
-    let factors = prime_factors(k);
-    // d!(factors);
-
-    let mut answers = BTreeSet::new();
-
-    for i in 0..3usize.pow(factors.len() as u32) {
-        let abc_ind = number_system(i, 3, factors.len());
-        // d!(i, abc_ind);
-        let mut abc = vec![1; 3];
-        for j in 0..factors.len() {
-            abc[abc_ind[j]] *= factors[j];
-        }
-        if abc[0] <= abc[1] && abc[1] <= abc[2] {
-            answers.insert(abc);
+    let dvs = divisors(k);
+    // d!(dvs);
+    let mut ans = 0;
+    for i in 0..dvs.len(){
+        for j in i..dvs.len() {
+            let a = dvs[i];
+            let b = dvs[j];
+            // d!(a, b);
+            if k / a < b { continue };
+            if k % (a * b) != 0 { continue };
+            let c = k / (a * b);
+            if c < b { continue };
+            // d!(a, b, c);
+            ans += 1;
         }
     }
-    println!("{}", answers.len());
+    println!("{}", ans);
 }
 
-fn number_system(n: usize, radix: usize, length: usize) -> Vec<usize> {
-    let mut n = n;
-    (0..length)
-        .map(|_| {
-            let x = n % radix;
-            n /= radix;
-            x
-        })
-        .collect()
+fn divisors(n: usize) -> Vec<usize> {
+    let mut dvs = vec![];
+    for i in 1..=n {
+        if i * i > n { break }
+        if n % i == 0 {
+            dvs.push(i);
+            if i * i != n { dvs.push(n / i)}
+        }
+    }
+    dvs.sort_unstable();
+    dvs
 }
