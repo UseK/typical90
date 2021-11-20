29eda6281c81361a8d2bdc554c251f0e007f7df2
UseK <use.k.james@gmail.com>
2021-11-21 04:09:06 +0900

    Accept 001 - Yokan Party again

--- src/bin/001.rs
+++ src/bin/001.rs
@@ -1,27 +1,32 @@
-//use competitive_tools_rust::d;
-use competitive_tools_rust::io::*;
-use competitive_tools_rust::search::bound;
+use std::iter::{once};
+
+use competitive_tools_rust::{
+    // d,
+    io::{parse_line, parse_tuple2, parse_values},
+    search::bound,
+};
+use itertools::Itertools;
 
 fn main() {
     let (n, l): (usize, usize) = parse_tuple2();
     let k: usize = parse_line();
     let a: Vec<usize> = parse_values(n);
-    let (_, ans) = bound(l as isize, 1, |i| judge(i as usize, k, l, &a));
-    println!("{}", ans);
-}
-
-fn judge(score: usize, k: usize, l: usize, a: &[usize]) -> bool {
-    let mut count = 0;
-    let mut current_pos = 0;
-    for i in 0..a.len() {
-        if a[i] - current_pos >= score {
-            current_pos = a[i];
-            count += 1;
-            //d!(i, current_pos, count);
-            if count == k {
-                return l - current_pos >= score;
+    let pieces: Vec<usize> = once(0).chain(a).chain(once(l)).tuple_windows().map(|(x, y)| y - x).collect();
+    // d!(pieces);
+    let (_ng_bound, ok_bound) = bound(l as isize, 1, |min_length| {
+        let min_length = min_length as usize;
+        let mut acc_length = 0;
+        let mut count = 0;
+        pieces.iter().for_each(|&a_item| {
+            acc_length += a_item;
+            if acc_length >= min_length {
+                count += 1;
+                acc_length = 0;
             }
-        }
-    }
-    false
+        });
+        // d!(min_length, count, acc_length);
+        // d!(count > k);
+        count > k
+    });
+    println!("{}", ok_bound);
 }
