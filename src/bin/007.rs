b2e046f80160bcd08ae87f315193dd41212ae3bc
UseK <use.k.james@gmail.com>
2021-11-27 05:27:12 +0900

    Accept 007 - CP Classes, again

--- src/bin/007.rs
+++ src/bin/007.rs
@@ -1,24 +1,28 @@
-//use competitive_tools_rust::d;
 use competitive_tools_rust::io::*;
-use competitive_tools_rust::search::bound;
+use competitive_tools_rust::{search::bound};
 
 fn main() {
     let n: usize = parse_line();
-    let mut a: Vec<usize> = parse_values(n);
+    let mut a: Vec<isize> = parse_values(n);
     a.sort_unstable();
     let q: usize = parse_line();
-
-    for _ in 0..q {
-        let b: usize = parse_line();
-        let (l, r) = bound(-1, a.len() as isize, |i| b <= a[i as usize]);
-        //d!(b, l, r);
-        let ans = if l.is_negative() {
-            a[r as usize] - b
-        } else if r as usize == a.len() {
-            b - a[l as usize]
+    let b: Vec<isize> = (0..q).map(|_| parse_line()).collect();
+    // d!(n, a, q, b);
+    for b_item in b {
+        let (le_ind, gt_ind) = bound(-1, a.len() as isize, |i| {
+            i >= a.len() as isize || i >= 0 && a[i as usize] >= b_item
+        });
+        // d!(b_item, le_ind, gt_ind);
+        let le_diff = if le_ind.is_negative() {
+            isize::MAX
         } else {
-            (a[r as usize] - b).min(b - a[l as usize])
+            (a[le_ind as usize] - b_item).abs()
         };
-        println!("{:?}", ans);
+        let gt_diff = if gt_ind == a.len() as isize {
+            isize::MAX
+        } else {
+            (a[gt_ind as usize] - b_item).abs()
+        };
+        println!("{}", le_diff.min(gt_diff));
     }
 }
