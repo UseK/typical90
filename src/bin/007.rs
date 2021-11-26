d6e5fdef1de5d70dfab5d271a03fdd35764b756e
UseK <use.k.james@gmail.com>
2021-11-27 06:01:12 +0900

    Accept 007 - CP Classes, again more fine way

--- src/bin/007.rs
+++ src/bin/007.rs
@@ -1,5 +1,5 @@
 use competitive_tools_rust::io::*;
-use competitive_tools_rust::{search::bound};
+use competitive_tools_rust::search::bound;
 
 fn main() {
     let n: usize = parse_line();
@@ -9,20 +9,10 @@ fn main() {
     let b: Vec<isize> = (0..q).map(|_| parse_line()).collect();
     // d!(n, a, q, b);
     for b_item in b {
-        let (le_ind, gt_ind) = bound(-1, a.len() as isize, |i| {
-            i >= a.len() as isize || i >= 0 && a[i as usize] >= b_item
-        });
-        // d!(b_item, le_ind, gt_ind);
-        let le_diff = if le_ind.is_negative() {
-            isize::MAX
-        } else {
-            (a[le_ind as usize] - b_item).abs()
-        };
-        let gt_diff = if gt_ind == a.len() as isize {
-            isize::MAX
-        } else {
-            (a[gt_ind as usize] - b_item).abs()
-        };
+        let (le_ind, gt_ind) = bound(0, (a.len() - 1) as isize, |i| a[i as usize] >= b_item);
+        // d!(le_ind, gt_ind);
+        let le_diff = (a[le_ind as usize] - b_item).abs();
+        let gt_diff = (a[gt_ind as usize] - b_item).abs();
         println!("{}", le_diff.min(gt_diff));
     }
 }
